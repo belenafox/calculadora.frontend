5b812f2615eb2c3eb5861d21268bbd6c378427b3
Roswil Ascanio Aranguren <rascanio@kdu.cl>
2023-04-21 20:54:28 -0400

    cambio de nombre del archivo js

---
+++ js/calculadora.js
@@ -0,0 +1,21 @@
+let result = $("#result");
+
+
+function insert(num) {
+  result.val(result.val() + num);
+}
+
+
+function clearInput() {
+  result.val("");
+}
+
+
+function calculate() {
+  let expression = result.val();
+  if (expression.indexOf('/0') !== -1) { // Verificar si hay una división entre cero
+    result.val('Error: División entre cero');
+    return;
+  }
+  result.val(eval(expression));
+}
